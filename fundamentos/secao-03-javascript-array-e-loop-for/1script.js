42cb7733fae08aa2b2ec8e9711dd266e2643fa14
Matheus Bandeira <maabann@gmail.com>
2023-03-28 21:25:19 -0300

    Exercicio 1, Lição 2

--- fundamentos/secao-03-javascript-array-e-loop-for/1script.js
+++ fundamentos/secao-03-javascript-array-e-loop-for/1script.js
@@ -1,4 +1,7 @@
-//#1
+/*1 - Elabore alguns códigos e imprima o resultado no console usando o console.log,
+um para cada operação aritmética básica. 
+Seu código deve ter duas variáveis, a e b, definidas no começo com os valores 
+que serão operados. Escreva códigos para:*/
 
 let a = 5;
 let b = 10;
@@ -10,3 +13,13 @@ console.log("Divisão:" + " " + (a / b));
 console.log("Resto da Divisão:" + " " + (a % b));
 console.log("Exponenciação:" + " " + (a ** b));
 
+/*2 - Utilize if/else para escrever um código que retorne o maior de dois números. 
+Defina, no começo do seu código, duas variáveis com os valores que serão comparados.*/
+
+let num1 = 40;
+let num2 = 80;
+
+if(num1 > num2){
+console.log("O número é maior");}
+else{
+console.log("O número é MENOR");}
