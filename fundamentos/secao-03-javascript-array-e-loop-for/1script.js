b8713faca61e164cf2a713f37a0039ccb027e9d7
Matheus Bandeira <maabann@gmail.com>
2023-03-28 21:11:39 -0300

    Exercicio 1, Lição 1

---
+++ fundamentos/secao-03-javascript-array-e-loop-for/1script.js
@@ -0,0 +1,12 @@
+//#1
+
+let a = 5;
+let b = 10;
+
+console.log("Soma:" + " " + (a + b));
+console.log("Subtração:" + " " + (a - b));
+console.log("Multplicação:" + " " + (a * b));
+console.log("Divisão:" + " " + (a / b));
+console.log("Resto da Divisão:" + " " + (a % b));
+console.log("Exponenciação:" + " " + (a ** b));
+
