0f73c35bcf419129f7597b0c1070c09a1d3baa94
Matheus Bandeira <maabann@gmail.com>
2023-03-29 18:39:36 -0300

    dia ok

--- fundamentos/secao-03-javascript-array-e-loop-for/1script.js
+++ fundamentos/secao-03-javascript-array-e-loop-for/1script.js
@@ -16,10 +16,21 @@ console.log("Exponenciação:" + " " + (a ** b));
 /*2 - Utilize if/else para escrever um código que retorne o maior de dois números. 
 Defina, no começo do seu código, duas variáveis com os valores que serão comparados.*/
 
-let num1 = 40;
-let num2 = 80;
+let num21 = 40;
+let num22 = 80;
 
-if(num1 > num2){
+if(num21 > num22){
 console.log("O número é maior");}
 else{
 console.log("O número é MENOR");}
+
+/*3 - Utilize if/else para escrever um código que retorne o maior de três
+números. Defina, no começo do seu código, três variáveis com os valores
+que serão comparados.*/
+
+let num31 = 10;
+let num32 = 20;
+let num33 = 30;
+let maiorNum;
+
+if(num31 > num32 && num31 > num33){console.log(num31 "- É o maior número.")}
